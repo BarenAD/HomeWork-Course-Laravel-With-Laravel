f6f97b849683f3496557b24873fd92ea381bd7d4
BarenAD <baren1998@gmail.com>
2019-12-07 23:23:08 +0700

    HomeWork 7. 2

---
+++ app/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    protected $table = 'test';
+    protected $fillable = ['text', 'is_enabled'];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+}
