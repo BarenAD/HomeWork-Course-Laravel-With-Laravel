459a1c9e251055d12e7e52735cbf93e5612b70da
BarenAD <baren1998@gmail.com>
2019-12-12 14:47:27 +0700

    Homework 8-9. 3

--- app/Http/Controllers/MegaController.php
+++ app/Http/Controllers/MegaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\PahanDebilIsNotUnderstandExercise;
 use App\Test;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,12 @@ class MegaController extends Controller
 {
     public function get_test_by_id($in_id)
     {
-        return response()->json(Test::find_by_id($in_id));
+        $response = Test::find_by_id($in_id);
+        if (isset($response)) {
+            return response()->json($response,200);
+        } else {
+            throw new PahanDebilIsNotUnderstandExercise("ROW IS NOT FOUND! BIG ASSS!");
+        }
     }
 
     public function get_test_by_text($in_text)
